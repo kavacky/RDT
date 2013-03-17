b55cb0aa909ba4e0ddd7a5fba496f9bfda2f6a33
Kavacky <kavackys@gmail.com>
2013-03-17 11:09:54 +0200

    v1

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(566, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(491, 200);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(362, 34);
+            this.progressBar1.TabIndex = 1;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(21, 29);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(429, 402);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(528, 309);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(46, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.Text = "pixel";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(525, 348);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(45, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.Text = "lvert";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(525, 394);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(42, 17);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.Text = "lhor";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Checked = true;
+            this.radioButton4.Location = new System.Drawing.Point(524, 439);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(51, 17);
+            this.radioButton4.TabIndex = 6;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "block";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(502, 496);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(41, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "56";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(565, 494);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(69, 20);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "56";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1176, 577);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            int w = 0;
+            int h = 0;
+            int count = 0;
+            string dir = "";
+
+            foreach (String fn in openFileDialog1.FileNames) {
+                //richTextBox1.Text = richTextBox1.Text + "\n" + fn;
+                Image t = Image.FromFile(fn);
+                //richTextBox1.Text = richTextBox1.Text + "\n" + a.Width + " x " + a.Height + "\n";
+                w = t.Width;
+                h = t.Height;
+                dir = System.IO.Path.GetDirectoryName(fn);
+                count++;
+                t.Dispose();
+            }
+            richTextBox1.AppendText("Count: " + count + "\n");
+            richTextBox1.AppendText("Size: " + w + "x" + h + "\n");
+            richTextBox1.AppendText("Dir: " + dir + "\n");
+
+            //fixed int[8] mask;
+            Random rand = new Random();
+            int[,] mask = new int[w, h];
+            richTextBox1.AppendText("Mask: " + w + "x" + h + "\n");
+
+
+            if (radioButton4.Checked)
+            {
+                // Blocks
+
+                int block_width = int.Parse(textBox1.Text);
+                int block_height = int.Parse(textBox2.Text);
+                int block_count_x = w / block_width;
+                int block_count_y = h / block_height;
+
+                richTextBox1.AppendText("Blocks: " + block_count_x + " x " + block_count_y + "\n");
+
+                int[,] blockmask = new int[block_count_x, block_count_y];
+                for (int y = 0; y < block_count_y; y++)
+                {
+
+                    for (int x = 0; x < block_count_x; x++)
+                    {
+                        //mask[x, y] = ;
+                        int rnd = rand.Next(0, count);
+                        blockmask[x, y] = rnd;
+                        //richTextBox1.AppendText("BM: " + x + " - " + y + " = " + rnd + "\n");
+                    }
+                    //break;
+                }
+
+                //richTextBox1.Text = "";
+                /*
+                //int blocksize = w / 816; // 4x4
+                //int blocksize = w / (816 / 4); // 4x4
+                int blocksize = w / 51;
+
+                
+                int[,] blockmask = new int[w / blocksize, h / blocksize];
+                for (int y = 0; y < h / blocksize; y++)
+                {
+
+                    for (int x = 0; x < w / blocksize; x++)
+                    {
+                        //mask[x, y] = ;
+                        int rnd = rand.Next(0, count);
+                        blockmask[x, y] = rnd;
+                        //richTextBox1.AppendText("Mask: " + x + "x" + y + " = " + rnd + "\n");
+                    }
+                    //break;
+                }*/
+
+                int mx = 0;
+                int my = 0;
+                for (int x = 0; x < w; x++)
+                {
+                    for (int y = 0; y < h; y++)
+                    {
+                        //mask[x, y] = ;
+                        //int rnd = rand.Next(0, count);
+                        /*int px = x / blocksize;
+                        int py = y / blocksize;
+
+                        if (px > w / blocksize)
+                        {
+                            px = (w / blocksize)-2;
+                        }
+
+                        if (py > h / blocksize)
+                        {
+                            py = (h / blocksize)-2;
+                        }
+
+                        px = 1;
+                        py = 1;*/
+
+                        int px = x / block_width;
+                        int py = y / block_height;
+
+                        if (px >= block_count_x)
+                        {
+                            px = block_count_x - 1;
+                        }
+                        if (px < 0)
+                        {
+                            px = 0;
+                        }
+
+                        if (py>= block_count_y)
+                        {
+                            py = block_count_y - 1;
+                        }
+                        if (py < 0)
+                        {
+                            py = 0;
+                        }
+
+                        /*px--;
+                        py--;
+                        if (px < 0) { px = 0; }
+                        if (py < 0) { py = 0; }
+                        px = 1 -1;
+                        py = 1 -1;
+
+                        if (px > mx) { mx = px; }
+                        if (py > my) { my = py; }*/
+
+                        //richTextBox1.AppendText("Get blocks: " + px + " x " + py + "\n");
+                        //richTextBox1.Text = "Get blocks: " + px + " x " + py + "\n";
+
+                        //px = 0; py = 0;
+                        int rnd = 0;
+                        try
+                        {
+                          //  int rnd = blockmask[px, py];
+                            //richTextBox1.AppendText("Try Get blocks: " + px + " x " + py + "\n");
+                            rnd = blockmask[px, py];
+                           // richTextBox1.AppendText("OK\n");
+                        }
+                        catch (System.IndexOutOfRangeException ex)
+                        {
+                            //richTextBox1.AppendText(ex.Message);
+                            richTextBox1.AppendText(px +  " x " + py + " - " + ex.Message +  "\n");
+                        }
+                        finally
+                        {
+                            //mask[x, y] = 1;
+                        }
+                        mask[x, y] = rnd;
+                        //richTextBox1.AppendText("Mask: " + x + "x" + y + " = " + rnd + "\n");
+                    }
+                    //break;
+                }
+                richTextBox1.AppendText("Get blocks: " + mx + " x " + my + "\n");
+            }
+
+            if (radioButton3.Checked)
+            {
+                // Lines horizontal
+                for (int y = 0; y < h; y++)
+                {
+                    int rnd = rand.Next(0, count);
+                    for (int x = 0; x < w; x++)
+                    {
+                        //mask[x, y] = ;
+                        mask[x, y] = rnd;
+                        //richTextBox1.AppendText("Mask: " + x + "x" + y + " = " + rnd + "\n");
+                    }
+                    //break;
+                }
+            }
+
+            if (radioButton2.Checked)
+            {
+                // Lines vertical
+                for (int x = 0; x < w; x++)
+                {
+                    int rnd = rand.Next(0, count);
+                    for (int y = 0; y < h; y++)
+                    {
+                        //mask[x, y] = ;
+                        mask[x, y] = rnd;
+                        //richTextBox1.AppendText("Mask: " + x + "x" + y + " = " + rnd + "\n");
+                    }
+                    //break;
+                }
+            }
+
+            if (radioButton1.Checked)
+            {
+                // Pixel
+                for (int x = 0; x < w; x++)
+                {
+                    for (int y = 0; y < h; y++)
+                    {
+                        //mask[x, y] = ;
+                        int rnd = rand.Next(0, count);
+                        mask[x, y] = rnd;
+                        //richTextBox1.AppendText("Mask: " + x + "x" + y + " = " + rnd + "\n");
+                    }
+                    //break;
+                }
+            }
+
+
+            //int[,] multiDimensionalArray1 = new int[w, h];
+            //Image final = Image
+            Bitmap fin = new Bitmap(w, h);
+
+            
+            int n = 0;
+            progressBar1.Maximum = w * h;
+            progressBar1.Value = 0;
+            foreach (String fn in openFileDialog1.FileNames)
+            {
+                Bitmap t = new Bitmap(fn);
+
+                
+                for (int x = 0; x < w; x++)
+                {
+                    for (int y = 0; y < h; y++)
+                    {
+                        
+                        if (mask[x, y] == n)
+                        {
+                            fin.SetPixel(x, y, t.GetPixel(x, y));
+                            progressBar1.Value = progressBar1.Value + 1;
+                            Application.DoEvents();
+                        }
+                        //mask[x, y] = ;
+                        //int rnd = rand.Next(0, count);
+                        //mask[x, y] = rnd;
+                        //richTextBox1.AppendText("Mask: " + x + "x" + y + " = " + rnd + "\n");
+                    }
+                    //break;
+                }
+
+
+                //richTextBox1.Text = richTextBox1.Text + "\n" + fn;
+               // Image t = Image.FromFile(fn);
+                //richTextBox1.Text = richTextBox1.Text + "\n" + a.Width + " x " + a.Height + "\n";
+                //w = t.Width;
+                //h = t.Height;
+                //dir = System.IO.Path.GetDirectoryName(fn);
+                //count++;
+                //t.Dispose();
+                t.Dispose();
+                n++;
+            }
+
+
+
+            //fin.SetPixel
+            
+            //richTextBox1.Text = openFileDialog1.FileNames.ToString();
+
+            richTextBox1.AppendText("Save to: " + dir + "/final.bmp" + "\n");
+            fin.Save(dir + "/final.bmp");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
